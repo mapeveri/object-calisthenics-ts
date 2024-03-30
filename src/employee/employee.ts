9cbcf6e8fc15c79940b3f9a11aa3e99af9890d9f
Martín Peveri <martinpeveri@gmail.com>
2024-03-30 17:43:00 +0100

    Rule: Only One Level Of Indentation Per Method

--- src/employee/employee.ts
+++ src/employee/employee.ts
@@ -7,17 +7,18 @@ export function addEmployee(
   department: string
 ): void {
   const employee = { id, name, salary, department };
-  if (!employeeExists(id)) {
-    if (department === "Engineering") {
-      salary += calculateBonus(salary, 0.1);
-    } else if (department === "HR") {
-      salary += calculateBonus(salary, 0.2);
-    }
-    employeeData.push(employee);
-    console.log("Employee added successfully!");
-  } else {
+  if (employeeExists(id)) {
     console.log("Error! Employee already exists.");
+    return;
   }
+
+  if (department === "Engineering") {
+    salary += calculateBonus(salary, 0.1);
+  } else if (department === "HR") {
+    salary += calculateBonus(salary, 0.2);
+  }
+  employeeData.push(employee);
+  console.log("Employee added successfully!");
 }
 
 export function employeeExists(id: number): boolean {
@@ -41,15 +42,16 @@ export function calculateTotalSalary(): number {
 
 export function removeEmployeeById(id: number): void {
   const index = employeeData.findIndex((employee) => employee.id === id);
-  if (index !== -1) {
-    if (employeeData[index].department !== "Management") {
-      employeeData.splice(index, 1);
-      console.log("Employee removed successfully!");
-    } else {
-      console.log("Error! Cannot remove a management employee.");
-    }
-  } else {
+  if (index === -1) {
     console.log("Error! Employee not found.");
+    return;
+  }
+
+  if (employeeData[index].department !== "Management") {
+    employeeData.splice(index, 1);
+    console.log("Employee removed successfully!");
+  } else {
+    console.log("Error! Cannot remove a management employee.");
   }
 }
 
@@ -66,24 +68,31 @@ export function updateEmployeeSalary(id: number, newSalary: number): void {
 export function getHighestPaidEmployeeByDepartment(department: string): any {
   let highestSalary = -1;
   let highestPaidEmployee = null;
-  for (const employee of employeeData) {
-    if (employee.department === department && employee.salary > highestSalary) {
-      highestSalary = employee.salary;
-      highestPaidEmployee = employee;
-    }
+
+  const employeesByDeparment = employeeData.filter(
+    (employee) => employee.department === department
+  );
+  for (const employee of employeesByDeparment) {
+    highestSalary = Math.max(highestSalary, employee.salary);
+    highestPaidEmployee =
+      highestSalary === employee.salary ? employee : highestPaidEmployee;
   }
+
   return highestPaidEmployee;
 }
 
 export function calculateAverageSalaryByDepartment(department: string): number {
   let totalSalary = 0;
   let count = 0;
-  for (const employee of employeeData) {
-    if (employee.department === department) {
-      totalSalary += employee.salary;
-      count++;
-    }
+
+  const employeesByDeparment = employeeData.filter(
+    (employee) => employee.department === department
+  );
+  for (const employee of employeesByDeparment) {
+    totalSalary += employee.salary;
+    count++;
   }
+
   return count > 0 ? totalSalary / count : 0;
 }
 
